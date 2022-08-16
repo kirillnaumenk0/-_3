e4c9b44aab0adc41ebc0a78578d616bf50442435
Kirill <kirillnaumenk0@mail.ru>
2022-08-16 21:19:36 +0600

    commit№2_end

--- №2/Program.cs
+++ №2/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿Console.WriteLine("Введите координаты первой точки");
+Console.WriteLine(" ");
+Console.WriteLine("x: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.WriteLine(" ");
+Console.WriteLine("x: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Pow((Math.Pow(x2 - x1, 2) +
+                         Math.Pow(y2 - y1, 2) +
+                         Math.Pow(z2 - z1, 2) *
+                                   1.0), 0.5);
+Console.WriteLine(res);
+//формула (double res) из интернета, 
+//которая была на семинаре(double res = Math.Sqrt(((x2| - x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2)); не сработала! 
--- №3/Program.cs
+++ №3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+
+    int res = i * i * i;
+    Console.WriteLine(res);
+}
